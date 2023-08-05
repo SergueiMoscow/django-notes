6eedae5e2af6e367cd7b2880da06dacf355711f3
Sergey <94781301+SergueiMoscow@users.noreply.github.com>
2023-08-05 14:31:51 +0300

    create test for shares

--- notes/Shares.py
+++ notes/Shares.py
@@ -20,47 +20,61 @@ class Shares:
     path = None
     user: User = None
     user_profile: UserProfile = None
+    last_charged_ymd = None
+    last_day_notes_charged = None
+    today = None
+    today_notes_charged = 0
 
     def __init__(self, user):
         self.user = user
         self.user_profile = UserProfile.objects.get(user=user)
-
-        pattern = re.compile(r'\d{4}-\d{2}-\d{2}_url\.json')  # шаблон имени файла
-        files = os.listdir(self.path)
-        matching_files = [f for f in files if pattern.match(f)]
-        print(matching_files)  # список файлов
-        for file in matching_files:
-            pass
+        self.today = datetime.now().strftime('%Y-%m-%d')
+        self.path = self.__get_path()
 
     def process(self):
-        self.path = self.__get_path()
         if self.path is None:
             return
-        last_charged, last_day_notes_charged = self.__get_last_charged()
-        last_charged_ymd = datetime.fromtimestamp(last_charged).strftime("%Y-%m-%d")
+        last_charged, self.last_day_notes_charged = self.__get_last_charged()
+        self.last_charged_ymd = datetime.fromtimestamp(last_charged).strftime("%Y-%m-%d")
 
-        matching_files = self.__get_matched_files(last_charged_ymd)
-        today = datetime.now().strftime('%Y-%m-%d')
-        today_notes_charged = 0
+        matching_files = self.__get_matched_files()
         for file in matching_files:
-            full_filename = os.path.join(self.path, file)
-            with open(full_filename, 'r') as f:
-                shared_notes = json.load(f)
-            counter_notes = 0
-            for shared_note in shared_notes:
-                if file[0: 10] == last_charged_ymd and counter_notes <= last_day_notes_charged:
-                    continue
-                shared_note['user'] = self.user
-                shared_note['private'] = True
-                Note.objects.create(**shared_note).save()
-                today_notes_charged += 1 if file[0: 10] == today else 0
+            last_day_notes_charged = self.last_day_notes_charged if self.last_charged_ymd == file[0: 10] else 0
+            last_day_notes_charged = self.__process_file(str(file), last_day_notes_charged)
+            if file[0: 10] == self.today:
+                self.today_notes_charged = last_day_notes_charged
 
-        self.__set_last_charged(today_notes_charged)
+        self.__set_last_charged()
 
-    def __get_matched_files(self, last_charged_ymd):
+    def __process_file(self, filename: str, notes_charged: int) -> int:
+        """
+        :param filename: only filename without path. Path is taken from self.path
+        :param notes_charged: how many notes to pass.
+        :return: notes_charged including previous (received with parameter)
+        """
+        full_filename = os.path.join(self.path, filename)
+        with open(full_filename, 'r') as f:
+            shared_notes = json.load(f)
+        counter_notes = 0
+        local_notes_charged: int = notes_charged
+        for shared_note in shared_notes:
+            counter_notes += 1
+            if counter_notes <= local_notes_charged:
+                continue
+            shared_note['user'] = self.user
+            shared_note['private'] = True
+            Note.objects.create(**shared_note).save()
+            local_notes_charged += 1
+        if filename[0: 10] == self.today:
+            self.today_notes_charged = local_notes_charged
+
+
+        return local_notes_charged
+
+    def __get_matched_files(self):
         pattern = re.compile(r'\d{4}-\d{2}-\d{2}_url\.json')
         files = os.listdir(self.path)
-        matching_files = list(filter(lambda f: pattern.match(f) and f > last_charged_ymd, files))
+        matching_files = list(filter(lambda f: pattern.match(f) and f > self.last_charged_ymd, files))
         return matching_files
 
     def __get_path(self):
@@ -78,6 +92,10 @@ class Shares:
         return path
 
     def __get_last_charged(self) -> tuple:
+        """
+        Reads last.txt with info (<timestamp>, <notes_count>)
+        :return: tuple(<timestamp>, <notes_count>)
+        """
         if self.path is None:
             return 0, 0
         full_filename = os.path.join(self.path, self.FILE_LAST_CHARGED)
@@ -86,14 +104,28 @@ class Shares:
         unix_date = '0'
         with open(full_filename, 'r') as f:
             data = f.read()
-            unix_date, today_notes_charged = data.split(',')
-        return int(unix_date), int(today_notes_charged)
+            unix_date, notes_charged = data.split(',')
+        return int(unix_date), int(notes_charged)
 
-    def __set_last_charged(self, today_notes_charged):
+    def __set_last_charged(self):
+        """
+        Saves number of notes shared and saved today
+        :return: number of bytes written
+        """
         if self.path is None:
             return 0
         full_filename = os.path.join(self.path, self.FILE_LAST_CHARGED)
         now = datetime.now().timestamp()
         with open(full_filename, 'w') as f:
-            unix_date = f.write(f'{str(int(now))},{str(today_notes_charged)}')
-        return int(unix_date)
+            bytes_written = f.write(f'{str(int(now))},{str(self.today_notes_charged)}')
+        return int(bytes_written)
+
+    # names of theese classes is requirements of unit tests
+    def _TestShared__set_last_charged(self):
+        return self.__set_last_charged()
+
+    def _TestShared__get_last_charged(self):
+        return self.__get_last_charged()
+
+    def _TestShared__process_file(self, filename, notes_charged):
+        return self.__process_file(filename, notes_charged)
---
+++ tests/test_shared.py
@@ -0,0 +1,74 @@
+import os
+import shutil
+from datetime import datetime
+
+from django.test import TestCase
+from django.contrib.auth.models import User
+
+from notes.Shares import Shares
+from notes.models import UserProfile, Note
+from hidden_config import TELEGRAM_FILES
+
+
+class TestShared(TestCase):
+
+    user = None
+    shares = None
+    username = 'unittest'
+    password = 'test123456'
+    userprofile = None
+
+    def setUp(self):
+        self.user = User.objects.create_user(username=self.username, password=self.password)
+        self.userprofile = UserProfile.objects.create(user=self.user, telegram=self.username)
+        self.client.login(username=self.username, password=self.password)
+        self.shares = Shares(self.user)
+        user_notes_dir = os.path.join(TELEGRAM_FILES, self.username)
+        if not os.path.exists(user_notes_dir):
+            os.mkdir(user_notes_dir, mode=777)
+
+    # def test_set_last_charged(self):
+    #     num_bytes = self.shares.__set_last_charged()
+    #     self.assertGreater(num_bytes, 0)
+
+    # def test_get_last_charged(self):
+    #     today, num = self.shares.__get_last_charged()
+    #     self.assertEqual(num, 0)  # add assertion here
+
+    @staticmethod
+    def notes_records_count():
+        return Note.objects.count()
+
+    def test_process_file(self):
+        """
+        Тестируем 2 новых объекта с телеграма
+        :return:
+        """
+        now = datetime.now()
+        filename_test = f'{now.strftime("%Y-%m-%d")}_url.json'
+        file_name_full_path = os.path.join(self.shares.path, filename_test)
+        source = os.path.join(os.path.dirname(__file__), 'data', 'test_2.json')
+        shutil.copy2(source, file_name_full_path)
+        self.assertEqual(0, self.notes_records_count())
+        records_added = self.shares.__process_file(filename_test, 0)
+        self.assertEqual(2, records_added)
+
+        self.shares.__set_last_charged()
+        self.assertEqual(2, self.notes_records_count())
+
+        today, num = self.shares.__get_last_charged()
+        self.assertEqual(num, 2)
+        # Добавляем ещё объект с телеграма
+        source = os.path.join(os.path.dirname(__file__), 'data', 'test_3.json')
+        shutil.copy2(source, file_name_full_path)
+        self.shares.__process_file(filename_test, num)
+        self.shares.__set_last_charged()
+        self.assertEqual(3, self.notes_records_count())
+        today, num = self.shares.__get_last_charged()
+        self.assertEqual(num, 3)  # add assertion here
+
+    def tearDown(self):
+        pass
+        # self.userprofile.delete()
+        # self.user.delete()
+
