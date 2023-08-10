660aeea35e10963df446bee9e0d5e3d3ddba1679
Sergey <94781301+SergueiMoscow@users.noreply.github.com>
2023-08-10 10:07:04 +0300

    add celery

--- django_notes/settings.py
+++ django_notes/settings.py
@@ -152,11 +152,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = '/static/'
-# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+if DEBUG:
 
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
+    STATICFILES_DIRS = (
+        os.path.join(BASE_DIR, 'static'),
+    )
+else:
+    STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
