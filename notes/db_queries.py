0b26e261edb22183caaa4e39e442d859914d1d4f
Сергей Сушков <sushkov_sv@ursip.ru>
2023-09-24 13:29:46 +0300

    remove celery

--- notes/db_queries.py
+++ notes/db_queries.py
@@ -2,12 +2,9 @@ import re
 
 from bleach import clean
 from django.contrib.auth.models import User
-from django.core.cache import cache
 from django.db.models import Q
 
 from notes.models import Note, Tag, UserProfile
-
-CACHE_PERIOD = 60 * 60
 
 
 def get_list(request):
@@ -29,12 +26,8 @@ def get_list(request):
     if user:
         query &= Q(user__id=user)
 
-    notes_cache = cache.get(query)
-    if notes_cache:
-        notes = notes_cache
-    else:
-        notes = Note.objects.filter(query).prefetch_related('user__userprofile').all()
-        cache.set(query, notes, CACHE_PERIOD)
+    notes = Note.objects.filter(query).prefetch_related('user__userprofile').all()
+
     for note in notes:
         note.tags = get_tags_by_note_id(note.id)
         note.body = clean(note.body, tags=['br', 'p', 'hr', 'a'])
@@ -44,12 +37,12 @@ def get_list(request):
     return notes
 
 
+def generate_cash_key(key: dict):
+    pass
+
+
 def get_note_by_id(note_id):
-    cache_key = {'note': note_id}
-    note = cache.get(cache_key)
-    if note is None:
-        note = Note.objects.get(id=note_id)
-        cache.set(cache_key, note, CACHE_PERIOD)
+    note = Note.objects.get(id=note_id)
     return note
 
 
@@ -59,17 +52,12 @@ def get_tags_by_substring(substr: str):
 
 
 def get_tags_by_note_id(note_id: int):
-    cache_key = f'tags_{note_id}'
-    tags_list = cache.get(cache_key)
-    if tags_list is None:
-        tags_list = Tag.objects.filter(note_id=note_id)
-        cache.set(cache_key, tags_list, CACHE_PERIOD)
+    tags_list = Tag.objects.filter(note_id=note_id)
     return tags_list
 
 
 def create_tag(tag: str, note_id: Note):
     Tag.objects.create(tag=tag, note_id=note_id)
-    cache.delete(f'tags_{note_id.id}')
 
 
 def replace_urls_with_links(text):
@@ -87,12 +75,8 @@ def replace_urls_with_links(text):
 
 
 def get_user_profile(user: User):
-    cache_key = {'user_profile': user.id}
-    user_profile = cache.get(cache_key)
-    if user_profile is None:
-        try:
-            user_profile = UserProfile.objects.get(user=user)
-        except UserProfile.DoesNotExist:
-            user_profile = None
-        cache.set(cache_key, user_profile, CACHE_PERIOD)
+    try:
+        user_profile = UserProfile.objects.get(user=user)
+    except UserProfile.DoesNotExist:
+        user_profile = None
     return user_profile
